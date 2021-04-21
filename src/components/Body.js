ca11c1a29add76023b9b83a7508ab1e00b5b858f
Lily Kong <lilyhk28@gmail.com>
2021-04-20 22:24:09 -0700

    created Projects component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,14 @@
 import '../App.css';
 import About from './About.js';
 import Experience from './Experience.js';
+import Projects from './Projects.js'
 
 function Body() {
   return (
     <div>
       <About/>
       <Experience/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+import '../App.css';
+
+function Projects() {
+  return (
+    <div></div>
+  )
+}
+
+export default Projects;
